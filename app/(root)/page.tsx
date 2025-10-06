f3a8569e4724e597ac093dc89b4426ab8765e87e
Kibe Kevin <kibekevin215@gmail.com>
2025-10-06 20:44:53 +0300

    Add shadcn and Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
 
 export default function Home() {
   return (
-    <>
-        <h1 className="heading">Home</h1>
+    <>  
+        <section className="pink_container pattern">
+            <h1 className="heading">Pitch Your Startup <br/> Connect With Entrepreneurs</h1>
+            <p className="sub_heading !max-3xl">Submit Ideas, Vote on Pitches, And Get Noticed in Virtual Competitions</p>
+        </section>
+        
     </>
   )
 }
