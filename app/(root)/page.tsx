8dced72bb62faab42d90057920b705fa3bd3881f
Kibe Kevin <kibekevin215@gmail.com>
2025-10-10 19:56:04 +0300

    Add sanity's live api functionality (uploads and updates data in real time/ no need to refresh the page for updates)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 
 
@@ -10,7 +10,9 @@ export default async function Home({searchParams}:{searchParams:Promise<{query?:
 
     const query = (await searchParams).query
 
-    const posts = await client.fetch(STARTUPS_QUERY)
+    //const posts = await client.fetch(STARTUPS_QUERY)
+
+    const { data:posts } = await sanityFetch({query:STARTUPS_QUERY});
 
   return (
     <>  
@@ -33,6 +35,8 @@ export default async function Home({searchParams}:{searchParams:Promise<{query?:
             </ul>
 
         </section>
+
+        <SanityLive/>
         
     </>
   )
