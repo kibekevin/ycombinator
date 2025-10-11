05faf95c489a0eaca6ae7b0fa039a49d053a291b
Kibe Kevin <kibekevin215@gmail.com>
2025-10-11 13:18:04 +0300

    Add Real-time search functionality with URL query params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,10 +9,9 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
 
     const query = (await searchParams).query
+    const params = {search:query || null}
 
-    //const posts = await client.fetch(STARTUPS_QUERY)
-
-    const { data:posts } = await sanityFetch({query:STARTUPS_QUERY});
+    const { data:posts } = await sanityFetch({query:STARTUPS_QUERY, params});
 
   return (
     <>  
