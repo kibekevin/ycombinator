4343f15cb05a05195164345fd12686658d4cd6e5
Kibe Kevin <kibekevin215@gmail.com>
2025-10-12 22:15:22 +0300

    Add Sanity Write Client functionality (increase the number of views each time one visits/reloads startup details page)

--- components/View.tsx
+++ components/View.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 import Ping from './Ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
+
 
 const View = async ({id}:{id:string}) => {
 
     const {views:totalViews} = await client.withConfig({useCdn:false}).fetch(STARTUP_VIEWS_QUERY, {id})
+
+
+    await writeClient.patch(id).inc({views: 1}).commit()
+    
 
   return (
     <div className='view-container'>
