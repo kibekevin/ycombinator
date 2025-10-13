31bcb5b4567e01897fa4b9e640a551f6ff1b8ad3
Kibe Kevin <kibekevin215@gmail.com>
2025-10-13 16:34:36 +0300

    Add create author/user to sanity database after authentication functionality

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -38,3 +38,16 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type == 'startup' && _id == $
     _id,
     views
 }`)
+
+
+
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`*[_type == author && id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
+}`)
